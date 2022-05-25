e43e64efbda2310e648a6b9ca0daadb93fe502cf
Victor Aquino <97565011+VictorSaruva@users.noreply.github.com>
2022-05-24 22:33:46 -0300

    #master

--- layout/aluno/script/aluno.js
+++ layout/aluno/script/aluno.js
@@ -3,17 +3,22 @@ const alunoJson = [
         id:1,
         nome:"Victor Hugo Saruva De Aquino",
         colegio:"Colegio 3as",
-        matricula: 12345,
+        matricula: "Matricula: 12345",
         provaOnline:"Não há provas disponíveis",
-        provaRealizadas:[{
-            data:"01/04/2022",
-            presenca:"Presente",
-            prova:"Prova Matemática",
-            turma:"2A (2022/1)",
-            turno:"Manhã"
-        }],
+        provaRealizadas:[
+            "01/04/2022",
+            "Presença: Presente",
+            "Prova Matemática",
+            "2A (2022/1)",
+            "Manhã"
+        ],
         atividadeExtras:[{
             "atividade" : "Java",
+        }],
+        materia:[{
+            matematica:"Matematica",
+            portugues:"Português",
+            ingles:"ingles"
         }],
         faltas:[{
             matematica:0,
--- layout/aluno/script/script.js
+++ layout/aluno/script/script.js
@@ -1,12 +1,38 @@
 const c = (el)=>document.querySelector(el)
 const cs = (el)=>document.querySelector(el)
 
+
 alunoJson.map((item, index)=>{
-    let alunoItem = c('.area-aluno').cloneNode(true)
+    let alunoItem = c('.area-aluno .nav-aluno ').cloneNode(true)
 
-    alunoItem.querySelector('.nome-aluno').innerHTML = item.nome
-    alunoItem.querySelector('.area-colegio').innerHTML = item.colegio
+   alunoItem.querySelector('.area-nome-colegio').innerHTML = item.colegio
+   alunoItem.querySelector('.area-nome-aluno').innerHTML = item.nome
+   alunoItem.querySelector('.area-matricula').innerHTML = item.matricula
 
-    c('.nome-aluno').append(alunoItem)
+    c('.nav-aluno').append(alunoItem)
 
 })
+alunoJson.map((item, index)=>{
+    console.log(item)
+    let provaRealizadaItem = c('.models-prova-realizada .card-area-prova-realizada').cloneNode(true)
+
+    provaRealizadaItem.querySelector('.area-data').innerHTML = item.provaRealizadas[0] 
+    provaRealizadaItem.querySelector('.area-presenca').innerHTML = item.provaRealizadas[1]   
+
+    c('.area-prova').append(provaRealizadaItem)
+
+})
+
+
+alunoJson.map((item, index)=>{
+    console.log('sdfd')
+    let provaItem = c('.conteiner-prova').cloneNode(true)
+
+    provaItem.querySelector('.area-realizar-prova').innerHTML = item.provaOnline
+   
+    c('.conteiner-prova').append(provaItem)
+
+})
+
+
+
